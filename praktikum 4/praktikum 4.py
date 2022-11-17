98ae0ad6302e13a0827ccefedb91be5fa80eeecd
nailaafri <nailaafriani123@gmail.com>
2022-11-17 22:15:40 +0700

    pengumpulan praktikum

---
+++ praktikum 4/praktikum 4.py
@@ -0,0 +1,12 @@
+Python 3.11.0 (main, Oct 24 2022, 18:26:48) [MSC v.1933 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> kalimat_starter = "pada suatu hari yang cerah"
+>>> karakter = "terdapat seorang perempuan cantik yang sangat menawan"
+>>> waktu = "di hari minggu pagi ia mendengar kabar bahwa ia akan dijodohkan oleh keluarganya"
+>>> story_plot = "bermula ketika ia sedang bersantai dan ibunya mengajaknya berbicara mengenai perjodohan tersebut"
+>>> tempat = "di depan teras rumah ia berbicara banyak mengenai hal tersebut"
+>>> second_character = "ibunya bercerita bahwa calon suaminya nanti adalah sosok yang baik dan bertanggung jawab"
+>>> usia = "dan usia calon suaminya itu juga tidak berjarak terlalu jauh dari perempuan tersebut hanya sekitar lima tahun"
+>>> pekerjaan = "pekerjaan yang di lakukan suaminya tersebut adalah programmer jadi bisa menjamin kehidupannya kelak"
+>>> f"{kalimat_starter} {karakter} {waktu} {story_plot} {tempat} {second_character} {usia} {pekerjaan}"
+'pada suatu hari yang cerah terdapat seorang perempuan cantik yang sangat menawan di hari minggu pagi ia mendengar kabar bahwa ia akan dijodohkan oleh keluarganya bermula ketika ia sedang bersantai dan ibunya mengajaknya berbicara mengenai perjodohan tersebut di depan teras rumah ia berbicara banyak mengenai hal tersebut ibunya bercerita bahwa calon suaminya nanti adalah sosok yang baik dan bertanggung jawab dan usia calon suaminya itu juga tidak berjarak terlalu jauh dari perempuan tersebut hanya sekitar lima tahun pekerjaan yang di lakukan suaminya tersebut adalah programmer jadi bisa menjamin kehidupannya kelak'
